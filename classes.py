d12078fbdaeb5ae6f4c87bd0eb31be1d2ea0011e
Burrno <121233764+Burrno@users.noreply.github.com>
2023-12-21 23:16:31 -0300

    estrutura inicial (#7)
    Co-authored-by: frtsgui <frtsgui@gmail.com>

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Network:
+    def __init__(self, n_nodes: int, n_layers: int):
+        self.n_nodes = n_nodes
+        self.n_layers = n_layers   r  rr
+class Layer(Network):
+    def __init__(self, n_nodes: int):
+        self.n_nodes = n_nodes
+        self.n_layers = 1
+
+class Node(Layer):
+    def __init__(self):
+        self.n_nodes = 1
+        self.n_layers = 1
+
+if __name__ == '__main__':
+    teste = Network(5,7)
+    print(teste.n_nodes)
+    print(teste.n_layers)
